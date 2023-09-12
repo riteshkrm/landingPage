df5f4a1db941d03915c9aad1c0e7c7a7eaa37424
Your Name <you@example.com>
2023-09-13 02:41:56 +0530

    Final comit

---
+++ Script/script.js
@@ -0,0 +1,10 @@
+// 'use strict'
+// const navRightSide = document.querySelector('.nav-right-side');
+// const hamburger = document.querySelector('.mobile-hamburger');
+// const close = document.querySelector('.mobile-close');
+
+
+// hamburger.addEventListener('click', (menu)=>{
+//     hamburger.classList.toggle('active')
+//     navRightSide.classList.toggle('active')
+// })
